8299af5072781e08795b863e989fbead0232e1f9
MyAeroCode <meteo_meteo@naver.com>
2020-05-10 21:19:47 +0900

    add: 액세스 토큰 취득방법 예제

---
+++ examples/01-get-access-token/index.ts
@@ -0,0 +1,27 @@
+import { TistoryApi } from "../../src";
+
+async function main() {
+    //
+    // 키를 사용하여 API 객체를 생성한다.
+    const api = new TistoryApi({
+        client: "YOUR_CLIENT_KEY",
+        secret: "YOUR_SECRET_KEY",
+    });
+
+    //
+    // 계정정보를 직접 입력하여 코드를 얻는다.
+    const code: string = await api.getCodeViaAccountInfo({
+        id: "TISTORY_ID",
+        pw: "TISTORY_PW",
+    });
+
+    //
+    // 코드를 사용하여 액세스 토큰을 얻는다.
+    const accessToken: string = await api.getAccessTokenViaCode(code);
+
+    //
+    // 얻은 코드와 액세스 토큰을 출력한다.
+    console.log("code", code);
+    console.log("accessToken", accessToken);
+}
+main();
