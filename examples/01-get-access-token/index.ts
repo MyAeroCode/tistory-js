47c00380341b583adab20f5a1aeed1f00b5fcfe9
MyAeroCode <meteo_meteo@naver.com>
2020-05-11 17:16:01 +0900

    update: 예제코드 수정

--- examples/01-get-access-token/index.ts
+++ examples/01-get-access-token/index.ts
@@ -1,20 +1,20 @@
-import { TistoryApi } from "../../src/v1";
+import { TistoryApi } from "../../v1";
+// or
+// const { TistoryApi } = require("../../v1");
 
+/**
+ * 환경변수에 등록된 키와 계정정보를 사용하여,
+ * 액세스 토큰을 얻는 예제입니다.
+ */
 async function main() {
     try {
         //
         // 키를 사용하여 API 객체를 생성한다.
-        const api = new TistoryApi({
-            client: "YOUR_CLIENT_KEY",
-            secret: "YOUR_SECRET_KEY",
-        });
+        const api = new TistoryApi();
 
         //
         // 계정정보를 직접 입력하여 코드를 얻는다.
-        const code: string = await api.getCodeViaAccountInfo({
-            id: "TISTORY_ID",
-            pw: "TISTORY_PW",
-        });
+        const code: string = await api.getCodeViaAccountInfo();
 
         //
         // 코드를 사용하여 액세스 토큰을 얻는다.
--- examples/02-modify-post/index.ts
+++ examples/02-modify-post/index.ts
@@ -1,23 +1,14 @@
-import { TistoryApi } from "../../src/v1";
+import { TistoryApi } from "../../v1";
+// or
+// const { TistoryApi } = require("../../v1");
 
+/**
+ * 게시글을 수정하는 예제입니다.
+ */
 async function main() {
     try {
-        //
-        // 키를 사용하여 API 객체를 생성한다.
-        const api = new TistoryApi({
-            client: "YOUR_CLIENT_KEY",
-            secret: "YOUR_SECRET_KEY",
-        });
-
-        //
-        // 계정정보를 직접 입력하여 코드를 얻는다.
-        const code: string = await api.getCodeViaAccountInfo({
-            id: "TISTORY_ID",
-            pw: "TISTORY_PW",
-        });
-
-        //
-        // 코드를 사용하여 액세스 토큰을 얻는다.
+        const api = new TistoryApi();
+        const code: string = await api.getCodeViaAccountInfo();
         const access_token: string = await api.getAccessTokenViaCode(code);
 
         //
