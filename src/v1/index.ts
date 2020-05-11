0468ae87178a9af4cf43b970f701b58c9500eea1
MyAeroCode <meteo_meteo@naver.com>
2020-05-11 17:04:55 +0900

    fix: (1.14.4) import, require support issue

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,10 +1,2 @@
-import { TistoryApi } from "./tistory-api";
-
-//
-// import support.
 export * from "./types";
 export * from "./tistory-api";
-
-//
-// require support.
-module.exports = TistoryApi;
